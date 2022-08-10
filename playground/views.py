b4289d196d8b46b23e17f2edc647f3f223634a87
fadellh <fadel.lukman.dev@gmail.com>
2022-08-10 10:37:44 +0700

    learn orm

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 # Create your views here.
 
 
+def calculate():
+    x = 1
+    y = 2
+    return x
+
+
 def say_hello(request):
-    return HttpResponse("Hello world")
+    # Get Exception
+    # product = Product.objects.get(pk=0).exists()
+    # # None
+    # exist = Product.objects.filter(pk=0).exists()
+
+    # Filter
+    # keyword=value
+    fill = Product.objects.filter(price__gt=20)
+    range = Product.objects.filter(price__range=(20, 30))
+    coll = Product.objects.filter(collection__id__range=(1, 2, 3))
+
+    return render(request, 't.html', {'name': 'Mosh'})
--- storefront/urls.py
+++ storefront/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+# hello
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls'))
+    path('playground/', include('playground.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
